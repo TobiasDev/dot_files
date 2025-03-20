13f61004128161f8311e3109f019f054e0649f7b
Tobias Dörrich <developer@tobiasdoerrich.com>
2025-03-20 11:19:36 +0100

    Added ignored file types to telescope

--- nvim/lua/config/telescope_config.lua
+++ nvim/lua/config/telescope_config.lua
@@ -1,3 +1,24 @@
 -- Telescope plugin
 
-require("telescope").setup()
+require("telescope").setup({
+    defaults = {
+        file_ignore_patterns = { 
+			".*.uid", 
+			".*.png", 
+			".*.jpg", 
+			".*.jpeg", 
+			".*.tscn", 
+			".*.mp3", 
+			".*.wav", 
+			".*.res", 
+			".*.theme", 
+			".*.translation", 
+			".*.ttf", 
+			".*.stylebox", 
+			".*.svg", 
+			".*.ico", 
+			".*.import", 
+			".*.godot", 
+		},
+    }
+})
